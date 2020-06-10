6efcb9ff4316bfc154f05f1db03005c17ea4494b
Rafael Abrantes <rafael.vieira.abranttes@gmail.com>
2020-06-09 23:57:34 -0300

    Implementando interface

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:buscadordegifs/ui/home_page.dart';
 void main(){
   runApp(MaterialApp(
     home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
   ));
 }
 
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -4,44 +4,92 @@ import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
 
-
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
   String _search;
   int _offset = 0;
 
-
   Future<Map> _getGif() async {
-      http.Response response;
+    http.Response response;
 
-      if(_search == null){
-        response = await http.get("https://api.giphy.com/v1/gifs/trending?api_key=BZGV4u4eWZP8snnr4Y4FyZN7wYHH6cZD&limit=25&rating=G");
-      }
-      else{
-        response = await http.get("https://api.giphy.com/v1/gifs/search?api_key=BZGV4u4eWZP8snnr4Y4FyZN7wYHH6cZD&q=$_search&limit=20&offset=$_offset&rating=G&lang=en");
-      }
+    if (_search == null) {
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/trending?api_key=BZGV4u4eWZP8snnr4Y4FyZN7wYHH6cZD&limit=25&rating=G");
+    } else {
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/search?api_key=BZGV4u4eWZP8snnr4Y4FyZN7wYHH6cZD&q=$_search&limit=20&offset=$_offset&rating=G&lang=en");
+    }
 
-      return json.decode(response.body);
-
+    return json.decode(response.body);
   }
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
 
-    _getGif().then((map){
+    _getGif().then((map) {
       print(map);
     });
   }
 
-
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Image.network(
+            "https://developers.giphy.com/static/img/dev-logo-lg.7404c00322a8.gif"),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.black,
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                  labelText: "Pesquise Aqui!",
+                  labelStyle: TextStyle(color: Colors.white),
+                  border: OutlineInputBorder()),
+              style: TextStyle(color: Colors.white, fontSize: 18.0),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Expanded(
+            child: FutureBuilder(
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.waiting:
+                  case ConnectionState.none:
+                    return Container(
+                      width: 200.0,
+                      height: 200.0,
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                        strokeWidth: 5.0,
+                      ),
+                    );
+                  default:
+                    if (snapshot.hasError) return Container();
+                    else return _createGifTable(context, snapshot);
+                }
+              },
+              future: _getGif(),
+            ),
+          ),
+        ],
+      ),
+    );
   }
+
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot){
+
+  }
+
+
 }
